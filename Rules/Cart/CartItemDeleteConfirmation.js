63be1d59ee22e8d888e04dbbd52def4e07c1da84
Nhat Doan <nhat.doan@sap.com>
2022-08-18 15:02:08 +0000

    enhance delete cart item

--- Rules/Cart/CartItemDeleteConfirmation.js
+++ Rules/Cart/CartItemDeleteConfirmation.js
@@ -7,17 +7,19 @@ export default function CartItemDeleteConfirmation(context) {
             context.executeAction({
                 'Name': '/ProductCatalog2/Actions/Cart/DeleteCartItem.action'}).then(
                 success => {
-                    context.executeAction({
-                        'Name': '/ProductCatalog2/Actions/Cart/NaviTo_CartDetail.action'
-                    })
+                    
                 },
                 failure => {
                     console.log(failure)
+                   
                 }
             )
 
         } else {
             console.log('User rejected')
+            context.executeAction({
+                'Name': '/ProductCatalog2/Actions/CloseModalComplete.action'
+            })
         }
     });
 }
