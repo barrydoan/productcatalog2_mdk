61ff4f26d6c96b734d9b0a6fdf84d081db1276e1
Nhat Doan <nhat.doan@sap.com>
2022-08-19 16:46:42 +0000

    add log for scan product detail

--- Rules/Scan/BarcodeScanDetail.js
+++ Rules/Scan/BarcodeScanDetail.js
@@ -1,28 +1,37 @@
 
 import { formatBarcode } from './BarcodeUtil/FormatBarcode';
 
-export default function BarcodeScanDetail(context) {
-    let pageProxy = context.getPageProxy();
-    var message = '';
-    var actionResult = context.getActionResult('BarcodeScannerResult');
-    var scannedResult = actionResult.data;
-    let formatedBarcode = formatBarcode(scannedResult)
-    context.read('/ProductCatalog2/Services/productcatalog2.service', 'Products', [], `$filter=UPC eq '${formatedBarcode}'`)
-    .then(results => {
+export default async function BarcodeScanDetail(context) {
+    try {
+        console.log('BarcodeScanDetail start')
+        console.time('BarcodeScanDetail')
+        let pageProxy = context.getPageProxy();
+        var message = '';
+        var actionResult = context.getActionResult('BarcodeScannerResult');
+        var scannedResult = actionResult.data;
+        let formatedBarcode = formatBarcode(scannedResult)
+        let results = await context.read('/ProductCatalog2/Services/productcatalog2.service', 'Products', [], `$filter=UPC eq '${formatedBarcode}'`)
         if (results && results.length) {
             let prod = results.getItem(0);
             pageProxy.setActionBinding(prod)
-            return pageProxy.executeAction('/ProductCatalog2/Actions/Product/NaviTo_ProductDetail.action')
+            await pageProxy.executeAction('/ProductCatalog2/Actions/Product/NaviTo_ProductDetail.action')
         }
         else {
             message = `Product with barcode: ${formatedBarcode} not found`
-            context.executeAction({
+            await context.executeAction({
                 'Name': '/ProductCatalog2/Actions/GenericToastMessage.action',
                 'Properties': {
                     'Message': message
                 }
             })
         }
-    })
+             
+    } catch (e) {
+        console.error(e);
+    } finally {
+        console.timeEnd('BarcodeScanDetail')
+        console.log('BarcodeScanDetail end')
+    }
+    
 
 }
